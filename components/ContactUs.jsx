f86ebc059d85fc466b107ea9a9744758a2bbbe5d
LairW3b <lair.w3b@gmail.com>
2023-04-14 11:56:10 -0600

    integración wa.me

--- components/ContactUs.jsx
+++ components/ContactUs.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useState} from 'react'
 import styles from '../src/styles/components/ContactUs.module.scss'
 
 const ContactUs = () => {
-  return (
+
+  const [name, setName] = useState('')
+  const [mnsj, setMnsj] = useState('')
+
+  const handleMessage = (e) => {
+    e.preventDefault() 
+    console.log(!name === '')
+    //if(!name === '') {
+      let url = 
+        `https://wa.me/522222385895?text=Hola asadero, Mi nombre es %20${name}%20%0A${mnsj}`
+      window.open(url)
+    //} else {
+      //console.log('Campo requerido')
+    //}
+
+    setName('')
+    setMnsj('')
+  }
+
+  return ( 
     <section className={styles.contact_content}>
       <div className={styles.content_from}>
         <h2>Contáctanos</h2>
@@ -11,26 +30,31 @@ const ContactUs = () => {
             type='text' 
             placeholder='Nombre' 
             className={styles.input}
-          />
-          <input 
-            type="email" 
-            placeholder='Email' 
-            className={styles.input}
+            value={name}
+            onChange={({target}) => setName(target.value)}
+            required
           />
           <textarea 
             name='mesaje' 
             id='1' 
             cols='30' 
             rows='10'
+            className={styles.textarea}
             placeholder='Estamos para servirte'
-          >
-
-          </textarea>
-          <input 
-            type="submit" 
-            value="Enviar" 
-            className={styles.form_btn}
+            value={mnsj}
+            onChange={({target}) => setMnsj(target.value)}
+            required
           />
+          <button 
+            className={styles.form_btn}
+            onClick={handleMessage}
+          >
+            Enviar
+          </button>
+          
+          
+          
+
         </form>
       </div>
     </section>
